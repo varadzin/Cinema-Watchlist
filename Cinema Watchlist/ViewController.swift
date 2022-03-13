3cfe390ddba73cc57a375195d3a7b4b3ae20048f
Frantisek Varadzin <varadzin@gmail.com>
2022-03-13 15:05:54 +0100

    Initial Commit

---
+++ Cinema Watchlist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinema Watchlist
+//
+//  Created by Frantisek Varadzin on 13.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
